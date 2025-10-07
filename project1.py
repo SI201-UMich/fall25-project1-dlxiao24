8a25181f2370d039fd9932fe5c151b499e839946
Daniel Xiao <newsap97@gmail.com>
2025-10-06 23:40:54 -0400

    load_penguins

--- project1.py
+++ project1.py
@@ -5,13 +5,15 @@ Email: dlxiao@umich.edu
 Collaborators: None
 '''
 
-
-
-
 import csv
+def load_penguins(csvfile):
+    with open(csvfile, newline = '') as penguinfile:
+        readsv = csv.reader(penguinfile)
+        for line in readsv:
+            print(line)
 
-with open('penguins.csv', newline = '') as penguinfile:
-    readsv = csv.reader(penguinfile)
-    for line in readsv:
-        print(line)
+def main():
+    load_penguins("penguins.csv")
 
+if __name__ == "__main__":
+    main()
