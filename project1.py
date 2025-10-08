32d9daf9ef76b4d7c095c752c527214b04da21fa
Daniel Xiao <newsap97@gmail.com>
2025-10-08 13:47:36 -0400

    species prop

--- project1.py
+++ project1.py
@@ -28,12 +28,24 @@ def isolate_island(penguin_data, island):
             island_penguin_data.append(penguin)
     return island_penguin_data
 
-
+def species_prop(island_penguin_data):
+    species_count = {}
+    total = 0
+    for penguin in island_penguin_data:
+        species_count[penguin['species']] = species_count.get(penguin['species'], 0) + 1
+        total += 1 
+    for key, value in species_count.items():
+        species_count[key] = "{:.2%}".format(value / total)
+    print(species_count)
+    return species_count
+    
 
 
 
 def main():
-    load_penguins("penguins.csv")
+    penguins_loaded = load_penguins("penguins.csv")
+    penguins_isolated = isolate_island(penguins_loaded, "Dream")
+    species_prop(penguins_isolated)
 
 if __name__ == "__main__":
     main()
